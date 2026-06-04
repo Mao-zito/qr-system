e642557fe012582a124394d6bd3ef535cc648c35
lyduana <tenzazangetsu2017@hotmail.com>
2026-06-04 10:34:29 -0500

    v13

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,31 +2,26 @@ import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-
 class Database:
+    _connection = None
 
     @classmethod
     def connect(cls):
-        DATABASE_URL = os.getenv("DATABASE_URL")
+        if cls._connection is None:
+            DATABASE_URL = os.getenv("DATABASE_URL")
 
-        if not DATABASE_URL:
-            raise Exception("DATABASE_URL no está configurada")
+            if not DATABASE_URL:
+                raise Exception("DATABASE_URL no está configurada")
 
-        conn = psycopg2.connect(
-            DATABASE_URL,
-            cursor_factory=RealDictCursor,
-            sslmode="require"
-        )
+            cls._connection = psycopg2.connect(
+                DATABASE_URL,
+                cursor_factory=RealDictCursor,
+                sslmode="require"
+            )
 
-        print("Conexión establecida")
-        return conn
+        return cls._connection
 
 
 def get_cursor():
-    conn = Database.connect()
-    return conn.cursor(), conn
-
-
-def get_cursor_simple():
     conn = Database.connect()
     return conn.cursor(), conn
